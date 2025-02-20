075b6d0cf5ccec5fb62420b37c0784a23aa4dbc5
prasanthi venturi <prasanthiventuri96@gmail.com>
2025-02-20 21:32:50 +0530

    Add files via upload

---
+++ indexs.js
@@ -0,0 +1,41 @@
+
+1.ans
+function function name(){
+    console.log ("Hello world")
+
+
+}
+functionname()
+var function name=()=> "Hello world"
+        console.log(function name())
+
+
+2.ans
+        var add =(a,b)=>a+b
+        console.log (function(add 2,4))
+
+ 3.ans
+        var square = n=>n*n;
+        console .log (square(4))
+
+ 4.ans
+        var function name =a=>a *a
+
+5.ans
+        var cube =a=>a*3
+        console.log(cube(2))
+        
+
+         var cube =a=>{
+            return a*3
+         }
+         console.log(cube(2))
+
+6.ans
+         var diff=(a,b)=>a-b
+         console.log(diff(2,3))
+
+7.ans
+         var luffy=()=>"Hello world"
+         console.log (luffy())
+
